2080de561718850b06ad67744b0ca80f661f0115
Oskar Korczak <o.korczak@gmail.com>
2020-12-29 08:31:55 +0000

    Add ZIO fibers desc

--- src/main/scala/com/zio/fiber/ZioFibersApp.scala
+++ src/main/scala/com/zio/fiber/ZioFibersApp.scala
@@ -5,6 +5,17 @@ import zio.console._
 import zio.duration._
 import zio.ZIO
 
+/**
+ * ZIO.sleep(), based on fibers (non-blocking), solves the problem of blocking Thread.sleep().
+ * ZIO fibers are run in ZIO context, which adds its own fibers management layer
+ * and therefore allows to utilize fibers in non-blocking manner.  
+ *
+ * Output:
+ * zio-default-async-2 start-1
+ * zio-default-async-3 start-2
+ * zio-default-async-5 end-1
+ * zio-default-async-6 end-2
+ */
 object ZioFibersApp extends zio.App {
 
   def zioTask(id: Int) =
